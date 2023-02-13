333a4cfb0b3e0c6e6839bbb4e62c996e3d41f7f1
Ankit Jha <jhaa11701@gmail.com>
2023-02-13 18:44:48 +0530

    fixed bug

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -36,6 +36,14 @@ const Hero = () => {
                         <BsGithub className='text-xl mx-auto mt-1 text-white ' />
                     </div>
                 </div>
+                <a href="https://linkfree.eddiehub.io/J-Ankit2020" target={'_blank'} class="relative inline-block text-lg group ml-4 ">
+                    <span class="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-[#409EBE] transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+                        <span class="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+                        <span class="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+                        <span class="relative">Visit All Links</span>
+                    </span>
+                    <span class="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0" data-rounded="rounded-lg"></span>
+                </a>
             </div>
             <div className="right w-1/3 hidden md:block">
                 <img src="/assets/pc.gif" alt="" className='w-full ' />
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,27 +7,32 @@ const NavBar = () => {
     const [isOpen, setOpen] = useState(false);
     return (
         <header className='flex justify-between p-4 items-center  shadow-md'>
-            <h1 className='logo left font-bold tracking-widest'>Ankit Jha</h1>
-            <nav className='md:hidden bg-yellow-300 z-20   fixed right-0 top-0 h-screen flex flex-col items-end '>
+            <h1 className='logo left font-bold tracking-widest flex justify-between items-center'>
+                <span className='w-10 h-10'>
+                    <img src="/assets/man.svg" alt="" className='w-full h-full' />
+                </span>
+                Ankit Jha</h1>
+            <nav className='md:hidden  z-20   fixed right-0 top-0 h-screen flex flex-col items-end '>
                 <div className=''>
                     <Cross toggled={isOpen} toggle={setOpen} />
                 </div>
-                <ul className={`md:hidden ${isOpen ? 'block' : 'hidden'} h-3/4 w-[50vw] flex flex-col justify-between items-center text-xl  animate__animated ${isOpen ? 'animate__slideInRight' : 'animate__slideOutRight'}`}>
-                    <li >Home</li>
-                    <li >About</li>
-                    <li>Skills</li>
-                    <li >Education</li>
-                    <li>Projects</li>
-                    <li >Contact Me</li>
+                <div className='h-full '>
+                    <ul className={`md:hidden ${isOpen ? 'block' : 'hidden'} h-1/3 w-[20vw] flex flex-col justify-between items-center text-xl  animate__animated ${isOpen ? 'animate__slideInRight' : 'animate__slideOutRight'} bg-violet-800 opacity-80 text-white`}>
+                        <Link to="" smooth="true"><li className='mr-3 hover:text-blue-500 cursor-pointer' >Home</li></Link>
+                        <Link to="about" smooth="true"><li className='mr-3 hover:text-blue-500 cursor-pointer' smooth={true}>About</li></Link>
+                        <Link to="skills" smooth="true"><li className='mr-3 hover:text-blue-500 cursor-pointer'>Skills</li></Link>
+                        <Link to="project" smooth="true"><li className='mr-3 hover:text-blue-500 cursor-pointer'>Projects</li></Link>
+                        <Link to="contact" smooth={true}><li className='mr-3 hover:text-blue-500 cursor-pointer'>Contact</li></Link>
 
-                </ul>
-            </nav>
+                    </ul>
+                </div>
+            </nav >
             <nav className='hidden md:block px-10 font-semibold '>
-                <ul className={`md:flex justify-around hidden`}>
-                    <Link to="" smooth={true}><li className='mr-3 hover:text-blue-500 cursor-pointer' >Home</li></Link>
-                    <Link to="about" smooth={true}><li className='mr-3 hover:text-blue-500 cursor-pointer' smooth={true}>About</li></Link>
-                    <Link to="skills" smooth={true}><li className='mr-3 hover:text-blue-500 cursor-pointer'>Skills</li></Link>
-                    <Link to="project" smooth={true}><li className='mr-3 hover:text-blue-500 cursor-pointer'>Projects</li></Link>
+                <ul className={`md:flex justify-around hidden text-lg`}>
+                    <Link to="" smooth="true"><li className='mr-3 hover:text-blue-500 cursor-pointer' >Home</li></Link>
+                    <Link to="about" smooth="true"><li className='mr-3 hover:text-blue-500 cursor-pointer' smooth={true}>About</li></Link>
+                    <Link to="skills" smooth="true"><li className='mr-3 hover:text-blue-500 cursor-pointer'>Skills</li></Link>
+                    <Link to="project" smooth="true"><li className='mr-3 hover:text-blue-500 cursor-pointer'>Projects</li></Link>
                     <Link to="contact" smooth={true}><li className='mr-3 hover:text-blue-500 cursor-pointer'>Contact</li></Link>
                 </ul>
             </nav>
